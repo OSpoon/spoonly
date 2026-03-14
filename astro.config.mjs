a8a6d530b719cb6c72b3e53e9ebccd481a60d953
OSpoon <zxin088@gmail.com>
2026-03-14 11:58:02 +0800

    feat: add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://spoonly.cn',
 	output: 'server',
 	adapter: netlify(),
 	integrations: [react(), keystatic()],
